1f826397e415bda6bef9fee206298346415b7193
Profitlol <struon2@gmail.com>
2014-10-27 23:47:06 -0500

    Update KHeap.java

--- KHeap.java
+++ KHeap.java
@@ -64,7 +64,7 @@ public class KHeap
 
     public void insert(int v) 
     {
-        currentSize++;
+        
         if (size() == heap.length) 
             resize();        
         int hole = currentSize;
@@ -75,7 +75,7 @@ public class KHeap
         x.parentPos = parentPosition(x); // that Math floor thing       
         while (x.key < heap[parentPosition(x)].key) 
             swap(x, heap[parentPosition(x)]);                
-
+        currentSize++;
     }
 
     public void swap(Node a, Node b) 
@@ -140,47 +140,78 @@ public class KHeap
             key = index;
         }
 
+        // this is needed to print the output. i'm not sure why, but i read it
+        // on the internet
+        @Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(key);
+		return sb.toString();
+	}
     }
-///////SHIT DOES NOT WORK DOWN HERE
+
+    // the micro-sec will vary from each run time. 
+    // 2 = shittiest since u have to traverse a lot
+    // at a certain point of K children it levels out & becomes 
+    // relatively same speed. this is what you should notice
     public static void main(String[] args) throws FileNotFoundException 
     {
-        ArrayList<String> input = new ArrayList<String>();
-        int count = 0;
-        try {
-            Scanner scan = new Scanner(new File("karyHeap-input.txt"));
-            //KHeap tester = new KHeap();
-            while(scan.hasNextLine())
-            {
-                String line = scan.nextLine();
-                input.add(line);
-                count++;
-            }
-            for(int i =2; i <= 10; i +=2)
-            {
-                KHeap k = new KHeap(i);
-                double start = System.nanoTime();
-                for(int j= 0;j < input.size(); j+=2)
+		String filepath = "karyHeap-input.txt";
+		KHeap inp = new KHeap();
+		inp.loadfile(filepath);
+		for (int i = 2; i <= 10; i += 2) 
                 {
-                    String s = input.get(j);
-                    String[] op = s.split(" ");
-                    if (op[j].compareTo("IN") == 0) 
-                    {
-                        k.insert(Integer.parseInt(op[j+1]));
-                    }
-                    if (op[j].compareTo("EX") == 0) 
-                    {
-                        Node n = k.extractMin();
-                        System.out.println(n);
-                    }
-                }
-                double end = System.nanoTime();
-		double elapsed = (end-start)/1000.0;
-		System.out.println(elapsed + " micro-sec \n");
-            }
+			System.out.println("For k=" + i + " children");
+			KHeap k = new KHeap(i);
+			double start = System.nanoTime();
+			for (String s : inp.input) 
+				inp.opRunner(s, k);			
+			double end = System.nanoTime();
+			double elapsed = (end-start)/1000.0;
+			System.out.println(elapsed + " micro-sec \n");
+		}
 
-        } catch (IOException e) {
-            System.out.println("Theres no file");
-        }
-    }
+	}
+        // i will put all the lines of input into a giant ArrayList
+        // then i will mess around with the elements in another function
+	public boolean loadfile(String path) 
+        {
+		input = new ArrayList<String>();
+		Scanner testfile;
+		int count = 0;
+		try {
+			testfile = new Scanner(new File("karyHeap-input.txt"));
+			while (testfile.hasNextLine()) 
+                        {
+				String line = testfile.nextLine();
+				input.add(line);
+				count++;
+			}
+			testfile.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+                //down here is just debugging statements
+		if (input.size() == count) {
+			// System.out.println("File loaded  " + count );
+			return true;
+		} else
+			// System.out.println("SOMETHING'S WRONG LOL. ");
+			return false;
+	}
+
+	// i noticed that  shit was getting crazy in the main function
+        // i needed this so i can do my comparisions without worrying about
+        // too many fucking brackets and parens
+	public void opRunner(String s, KHeap k) throws FileNotFoundException {
+		String[] op = s.split(" ");
+		if (op[0].compareTo("IN") == 0) {
+			k.insert(Integer.parseInt(op[1]));
+		}
+		if (op[0].compareTo("EX") == 0) {
+			Node n = k.extractMin();
+			System.out.println(n);
+		}
+	}
 
 }
