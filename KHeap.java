6f156392f931bbe9c890b92f09ea02ea5afecc61
Profitlol <Profitlol@users.noreply.github.com>
2014-10-29 12:43:15 -0500

    Update KHeap.java

--- KHeap.java
+++ KHeap.java
@@ -1,329 +1,218 @@
-package avltree;
-import java.io.*;
+package kheap;
 import java.util.*;
+import java.io.*;
 /**
  *
  * @author Steven
  */
-public class AVLTree {
+public class KHeap 
+{
+    public Node[] heap;
+    public int currentSize;
+    public int k;
+    List<String> input; // using this for output
+    public int eCount = 0; // possible global counter for swtich cases
+
+    public KHeap(int children) 
+    {
+        k = children;
+        heap = new Node[100]; // make super big 100000f
+        currentSize = 0;
+    }
+
+    public KHeap()
+    {
+    }
+    //getting Node x's n(th) child 
+    public Node getChild(Node x, int n) 
+    {
+        if (n < k && n >= 0) 
+            return heap[k * x.pos + n];         
+        else 
+            return null;        
+    }
+
+    public int parentPosition(Node x) 
+    {
+        return (int) Math.floor((x.pos - 1) / k);
+    }
+
+    // if arr is full. i'm just making it 2x bigger
+    public void resize()  
+    {
+        heap = Arrays.copyOf(heap, (heap.length * 2));
+    }
+
+    public Node minChild(Node x) 
+    {
+        int min = x.key;
+        int minPos = 0;
+        // " i " will be the child's index
+        for (int i = x.pos + 1; i <= (k * x.pos) + k; i++) 
+        {
+            if (heap[i] != null) 
+            {
+                if (heap[i].key < min) 
+                {
+                    min = heap[i].key;
+                    minPos = i;
+                }
+            }
+        }
+        return heap[minPos];
+    }
+
+    public void insert(int v) 
+    {        
+        if (size() == heap.length) 
+            resize();        
+        int hole = currentSize;
+        Node x = new Node(v);
+        // sifting up
+        heap[hole] = x;
+        x.pos = hole;
+        x.parentPos = parentPosition(x); // that Math floor thing       
+        while (x.key < heap[parentPosition(x)].key) 
+            swap(x, heap[parentPosition(x)]);                
+        currentSize++;
+    }
+
+    public void swap(Node a, Node b) 
+    {
+        int tempPos, tempPar;
+        tempPos = b.pos;
+        tempPar = b.parentPos;
+        heap[b.pos] = a;
+        heap[a.pos] = b;
+        b.pos = a.pos;
+        b.parentPos = a.parentPos;
+        a.pos = tempPos;
+        a.parentPos = tempPar;
+    }
+
+    public int size() 
+    {
+        return currentSize;
+    }
+
+    public void siftDown(int d) 
+    {
+        int temp = d;
+        // while the minimum child's value is  < current key
+        while (minChild(heap[d]).key < heap[d].key) 
+        {
+            temp = minChild(heap[temp]).pos;
+            swap(heap[temp], minChild(heap[temp]));
+        }
+    }
     
-    public Node root;
-    List<String> input; // again using this for the output
+    public Node extractMin() throws FileNotFoundException 
+        {
+            //long startTime = System.nanoTime();
+            Node min = heap[0];
+            heap[0] = heap[currentSize - 1];
+            heap[0].pos = 0;
+            heap[0].parentPos = 0;
+            heap[currentSize - 1] = null;
+            currentSize--;
+            siftDown(0);
+            //long endTime = System.nanoTime();
+//            boolean DEBUG = true; // w hat?
+//            if (DEBUG) {
+//                System.out.println(min + " new min = " + heap[0]);
+//            }
+//            if (DEBUG) {
+//                System.out.println("ExtractMin Time = " + ((endTime - startTime) / 1000));
+//            }
+            return min;
+        } 
 
     public class Node 
     {
+        int parentPos;
+        int pos;
         int key;
-        int bf;
-        Node left;
-        Node right;
-        Node parent;
-        boolean heightInc;
-        int height;
+        int val; // do i need this
 
-        public Node(int k, Node l, Node r) 
+        public Node(int index) 
         {
-            key = k;
-            left = l;
-            right = r;
-            height = 0;
+            key = index;
         }
 
-        public Node(int num) 
-        {
-            this(num, null, null); // children, left, right 
-        }
-    }
-
-    public AVLTree() 
-    {
-        root = null;
-    }      
-
-    public int height(Node n) 
-    {
-        if (n.left == null && n.right == null) 
-            return 0;         
-        else if (n.left == null) 
-            return 1 + height(n.right);         
-        else if (n.right == null) 
-            return 1 + height(n.left);         
-        else 
-            return 1 + maximum(height(n.left), height(n.right));        
-    }
-
-    public int maximum(int x, int y) 
-    {
-        if (x >= y) 
-            return x;         
-        else 
-            return y;        
-    }
-
-    public void getBalanceFactor(Node b) 
-    {
-        b.bf = height(b.left) - height(b.right);
-    }
-
-    public void insert(int key) 
-    {
-        root = insert(key, root);
-    }
-
-    // insert int key onto node z
-    public Node insert(int key, Node t) 
-    {
-        if (t == null) 
-            t = new Node(key, null, null);        
-        else if (key < t.key) 
-        {
-            Node p = t;
-            t.left = insert(key, t.left);
-            t.left.parent = p;
-            if (t.bf == 2) 
-            {
-                if (key < t.left.key) 
-                    t = rightRotate(t);                
-                else 
-                    t = lrRotate(t);              
-            }
-        } 
-        else if (key > t.key) 
-        {
-            Node p = t;
-            t.right = insert(key, t.right);
-            t.right.parent = p;
-            if (t.bf == 2) 
-            {
-                if (key > t.right.key) 
-                    t = leftRotate(t);                
-                else 
-                    t = rlRotate(t);          
-            }
-        } 
-        else         
-            t.height = maximum(height(t.left), height(t.right));        
-        return t;
-    }
-
-    // moving node position to another node's position
-    public void transplant(Node x, Node y) 
-    {
-        if (x.parent == null) 
-            x = y;         
-        else if (x.parent.left == x) 
-            x.parent.left = y;        
-        else 
-            x.parent.right = y;        
-        if (y != null) 
-            y.parent = x.parent;        
-    }
-
-    public Node rightRotate(Node x) 
-    {
-        Node y = x.left;
-        transplant(y, y.right);
-        transplant(x, y);
-        y.right = x;
-        x.parent = y;
-        return y;
-    }
-
-    public Node leftRotate(Node x) 
-    {
-        Node y = x.right;
-        transplant(y, y.left);
-        transplant(x, y);
-        y.left = x;
-        x.parent = y;
-        return y;
-    }
-
-    public Node lrRotate(Node x) 
-    {
-        leftRotate(x.left);
-        return rightRotate(x);
-    }
-
-    public Node rlRotate(Node x) 
-    {
-        rightRotate(x.right);
-        return leftRotate(x);
-    }
-    
-    // probably dont need
-    public Node minValue(Node x) 
-    {
-        while (x.left != null) 
-            x = x.left;        
-        return x;
-    }
-
-    // probably dont need
-    public Node maxValue(Node x) 
-    {
-        while (x.right != null) 
-            x = x.right;        
-        return x;
-    }
-    
-    // the logic happens after searchKey completed
-    // i.e. search for this key
-    public boolean search(Node x, int k) 
-    {
-        if (x == null) 
-            return false;       
-        if (x.key == k) 
-            return true;        
-        if (x.key > k) 
-            return search(x.left, k);         
-        else 
-            return search(x.right, k);        
-    }
-    
-    public Node searchKey(Node x, int y)
-    {
-        if (y > x.key)
-            return searchKey(x.right, y);
-        if (y < x.key)
-            return searchKey(x.left, y);
-        if (y == x.key)
-            return x;
-        return null;
-    }
-
-    public Node successor(Node x) 
-    {
-        if (x.right != null) 
-        {
-            Node y = x.right;
-            while (y.left != null) 
-                y = y.left;            
-            return y;
-        } 
-        else 
-        {
-            Node y = x.parent;
-            while (y != null && x == y.right) 
-            {
-                x = y;
-                y = y.parent;
-            }
-            return y;
-        }
-    }
-
-    public Node predecessor(Node x) 
-    {
-        if (x.left != null) 
-        {
-            Node y = x.left;
-            while (y.right != null) 
-                y = y.right;            
-            return y;
-        }
-        Node y = x.parent;
-        while (y != null && x == y.left) 
-        {
-            x = y;
-            y = y.parent;
-        }
-        return y;
-
-    }
-
-    public int rank(Node x, int k) 
-    {
-        if (x == null) 
-            return 0;        
-        if (k < x.key) 
-            return rank(x.left, k);        
-        if (k == x.key) 
-            return size(x.left) + 1;        
-        return size(x.left) + 1 + rank(x.right, k);
-    }
-
-    public int size(Node x) 
-    {
-        if (x == null)
-            return 0;
-        else
-            return size(x.left) + size(x.right) + 1;        
-    }
-
-    public Node select(Node x, int i) 
-    {
-        if (x == null) 
-            return null;        
-        if (size(x.left) >= i) 
-            return select(x.left, i);        
-        if (size(x.left) + 1 == i)
-            return x;        
-        return select(x.right, i - 1 - (size(x.left)));
-    }
-    
-    //doing same methods like KHeap
-    public static void main(String[] args) 
-    {		
-		String filepath = "AVLtree-input.txt";
-		AVLTree inp = new AVLTree();
-		inp.loadfile(filepath);
-		AVLTree avl = new AVLTree();
-		double startTime = System.nanoTime();
-		for (String s : inp.input) 
-			inp.opRunner(s, avl);		
-		double endTime = System.nanoTime();
-		double elapsed = endTime - startTime;
-		System.out.println(elapsed/1000.0 + "micro-sec");
+        // this is needed to print the output. i'm not sure why, but i read it
+        // on the internet
+        @Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(key);
+		return sb.toString();
 	}
+    }
 
+    // the micro-sec will vary from each run time. 
+    // 2 = shittiest since u have to traverse a lot
+    // at a certain point of K children it levels out & becomes 
+    // relatively same speed. this is what you should notice
+    public static void main(String[] args) throws FileNotFoundException 
+    {
+		String filepath = "karyHeap-input.txt";
+		KHeap inp = new KHeap();
+		inp.loadfile(filepath);
+		for (int i = 2; i <= 10; i += 2) 
+                {
+			System.out.println("For k=" + i + " children");
+			KHeap k = new KHeap(i);
+			double start = System.nanoTime();
+			for (String s : inp.input) 
+				inp.opRunner(s, k);			
+			double end = System.nanoTime();
+			double elapsed = (end-start)/1000.0;
+			System.out.println(elapsed + " micro-sec \n");
+		}
+
+	}
+        // i will put all the lines of input into a giant ArrayList
+        // then i will mess around with the elements in another function
 	public boolean loadfile(String path) 
         {
 		input = new ArrayList<String>();
-		Scanner filescn;
+		Scanner testfile;
 		int count = 0;
 		try {
-			filescn = new Scanner(new File("AVLtree-input.txt"));
-			while (filescn.hasNextLine()) 
+			testfile = new Scanner(new File("karyHeap-input.txt"));
+			while (testfile.hasNextLine()) 
                         {
-				String line = filescn.nextLine();
+				String line = testfile.nextLine();
 				input.add(line);
 				count++;
 			}
+			testfile.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-                // again debugging statements
+                //down here is just debugging statements
 		if (input.size() == count) {
-			// System.out.println("File successfully loaded " + count );
+			// System.out.println("File loaded  " + count );
 			return true;
 		} else
-			// System.out.println("File load unsuccessfull. ");
+			// System.out.println("SOMETHING'S WRONG LOL. ");
 			return false;
 	}
 
-        /// fix print outs here, debug comments are tehre
-	public void opRunner(String s, AVLTree avl) 
-        {
-		String[] op = s.split(" ");	
-		if (op[0].compareTo("IN") == 0) 
-			avl.insert(Integer.parseInt(op[1]));
-		if (op[0].compareTo("SC") == 0) 
-                {
-			int y = Integer.parseInt(op[1]);
-                        Node suc = avl.successor(avl.searchKey(avl.root, y));
-			System.out.println("Successor of: " + y + " = "+ suc.key);
+	// i noticed that  shit was getting crazy in the main function
+        // i needed this so i can do my comparisions without worrying about
+        // too many fucking brackets and parens
+	public void opRunner(String s, KHeap k) throws FileNotFoundException {
+		String[] op = s.split(" ");
+		if (op[0].compareTo("IN") == 0) {
+			k.insert(Integer.parseInt(op[1]));
 		}
-		if (op[0].compareTo("SE") == 0) 
-                {
-			int k = Integer.parseInt(op[1]);
-			Node sel = avl.select(avl.root,k);
-			System.out.println(k +"th smallest key is: " + sel.key);
-		}
-		if (op[0].compareTo("RA") == 0) 
-                {
-			int i = avl.rank(avl.root, Integer.parseInt(op[1]));
-			System.out.println("Rank of " + op[1] + " = "+ i);
+		if (op[0].compareTo("EX") == 0) {
+			Node n = k.extractMin();
+			System.out.println(n); 
+                        // made a global eCount variable 
+                        //for switch later
 		}
 	}
-    
+
 }
